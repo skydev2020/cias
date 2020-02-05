acd6981c3b82899a2c2270f1eb7486b0b963b88d
unknown <skyteam2019@outlook.com>
2020-02-05 14:49:53 -0500

    update main header

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -12,7 +12,7 @@
     <!-- Ionicons 2.0.0 -->
     <link href="<?php echo base_url(); ?>assets/bower_components/Ionicons/css/ionicons.min.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
-    <link href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.css" rel="stylesheet" type="text/css" />
     <!-- AdminLTE Skins. Choose a skin from the css/skins 
          folder instead of downloading all of them to reduce the load. -->
     <link href="<?php echo base_url(); ?>assets/dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
