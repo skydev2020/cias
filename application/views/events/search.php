f16835307092762b8173decd41574137ab0d834f
unknown <skyteam2019@outlook.com>
2020-02-11 07:48:46 -0500

    Search Text Box Update

--- application/views/events/search.php
+++ application/views/events/search.php
@@ -5,8 +5,8 @@
             <div class="col-12">
                 <form action="<?php echo base_url() ?>search?"  id="searchList" class="justify-content-center d-flex">
                     <!-- <input class="form-control search" type="text" name="q" value="<?php echo $q; ?>" placeholder="Start typing..."> -->
-                    <div class="input-group md-form form-sm form-2 pl-0 w-50">
-                        <input class="form-control my-0 py-1 query-text" name="q" value="<?php echo $q; ?>"  type="text" placeholder="Start typing..." aria-label="Search">
+                    <div class="input-group md-form form-sm form-2 pl-0 search-panel">
+                        <input class="form-control my-0 py-1 query-text font-size-18" name="q" value="<?php echo $q; ?>"  type="text" placeholder="Start typing..." aria-label="Search">
                         <div class="input-group-append">
                             <button class="input-group-text search-btn"><i class="fa fa-search text-white font-size-24"
                                 aria-hidden="true"></i></button>
@@ -26,7 +26,7 @@
             {
         ?>
         <div class="row">
-            <div class="col-12 text-center event-cell font-size-24">
+            <div class="col-12 event-cell font-size-24">
                 <a href="<?php echo base_url() ?>score?id=<?php echo $event->id ?>"><?php echo $event->name ?></a>
             </div>            
         </div>                    
